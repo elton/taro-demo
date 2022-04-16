e9a48137c43ba130a73be36b548daffac2137fce
Elton Zheng <elton.zheng.sh@gmail.com>
2022-04-16 21:44:44 +0800

    Initialize the project.

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,22 +1,11 @@
 import { Text, View } from '@tarojs/components';
-import { Component } from 'react';
 
-export default class Index extends Component {
-  componentWillMount() {}
+const Index = () => {
+  return (
+    <View className='index'>
+      <Text className='text-3xl text-green-700 m-4'>Hello world!</Text>
+    </View>
+  );
+};
 
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return (
-      <View className='index'>
-        <Text className='text-3xl text-green-700 m-4'>Hello world!</Text>
-      </View>
-    );
-  }
-}
+export default Index;
